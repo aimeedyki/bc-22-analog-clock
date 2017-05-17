41b2e3465e238b7065fc4b8d22fffebbf8a7ea27
Amarachi <akuwudike.a@gmail.com>
2017-05-17 19:12:03 +0100

    modified server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var express = require('express');  
-var app = express();  
+var app = express(); 
+require('dotenv').config() 
 
  
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {  
-  res.sendFile('C:/Users/ARAWN/clock/public/login.html');
+  res.sendFile(process.env.rootPath + '/public/login.html');
 });
 
 app.get('/signup', function (req, res){
 res.sendFile('C:/Users/ARAWN/clock/public/signup.html')
 });
 
-app.listen(process.env.PORT || 3000,function(){  
+app.listen(process.env.PORT || 3000, function(){  
 console.log('Example app listening on port 3000!')
 })
